8785ad160dbd843a924f44aed7e15f51d69cb5c2
Zach <zach@hackazach.net>
2012-03-04 11:02:30 -0500

    process player throw

--- rock_scissors_paper.rb
+++ rock_scissors_paper.rb
@@ -10,5 +10,19 @@ before do
 end
 
 get '/throw/:type' do
+	player_throw = params[:type].to_sym
+	if !@throws.include? player_throw
+		halt 403, "Sorry, throw #{:type} is not a valid move"
+	end
 
+	#select random throw for app
+	computer_throw = @throws.sample()
+
+	if player_throw == computer_throw
+		"Tie. Try again!"
+	elsif computer_throw == @defeat[player_throw]
+		"Good job, you WIN"
+	else
+		"Sorry. Computer beat your #{player_throw} with #{computer_throw}"
+	end
 end
