09ab7ad0576aaaf326373f1d3832a2e1df572189
Zach <zach@hackazach.net>
2012-03-08 17:57:07 -0500

    not_found handler

--- minimal.rb
+++ minimal.rb
@@ -1,10 +1,24 @@
 require 'sinatra'
 
+
+before '/' do
+	@name = 'robot sinatra!'
+end
+
 get '/' do
-	@name = 'zach'
+	@name = 'zach' if @name.nil?
 	erb :default
+end
+
+after '/' do
+	'see you later!'
 end
 
 get '/bad' do
 	halt 500
+end
+
+not_found do
+	content_type :txt
+	'whoa sorry.'
 end
