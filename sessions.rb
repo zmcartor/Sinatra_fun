6f2d9ff45f70adef6f02373dc7bb115252be800e
Zach <zach@hackazach.net>
2012-03-09 17:44:03 -0500

    attachments /novel route.

--- sessions.rb
+++ sessions.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 
 configure do
 	enable :sessions
+	set :session_secret , "blargggghhh"
 end
 
 get '/' do
@@ -13,4 +14,11 @@ end
 get '/session' do
 	content_type :txt
 	session[:blah]
+end
+
+#send an attachment. the file type will be used to set the Content-Disposition header
+
+get '/novel' do
+	attachment "OHHAI.txt"
+	"OH HAI, how are you??"
 end
